ec4c32f10426813acd4a01979538339690df5bb8
fOppenheimer <Fatma.Krueger@T-Systems.com>
2021-05-12 12:35:16 +0200

    finished first page

--- src/components/pdf-generater.component.tsx
+++ src/components/pdf-generater.component.tsx
@@ -24,14 +24,14 @@ import React from 'react';
 import '../i18n';
 import { useTranslation } from 'react-i18next';
 
-import { jsPDF } from "jspdf";
+import { jsPDF, TextOptionsLight } from "jspdf";
 
-import logo from '../assets/images/dgca_issuance_web.png';
+import logo from '../assets/images/EU_logo_big.png';
 
 import { propTypes } from 'qrcode.react';
 import { EUDGC, RecoveryEntry } from '../generated-files/dgc-combined-schema';
 
-    const usePdfGenerator = (qrCodeCanvasElement: any, eudgc:EUDGC | undefined) => {
+const usePdfGenerator = (qrCodeCanvasElement: any, eudgc: EUDGC | undefined) => {
 
     //A4 210 x 297 mm or 2480 x 3508 pixels or 595 × 842 points
     //A6 105 x 74 mm or 1240 x 1748 pixels or 298 × 420 points
@@ -49,6 +49,9 @@ import { EUDGC, RecoveryEntry } from '../generated-files/dgc-combined-schema';
     // let lblLength = canvasWidth/2 - 3;
 
     const lineHeight = 14;
+    const fontSize = 11;
+    const headerLineHeight = 22;
+    const headerFontSize = 18;
     const lblLength = a6width / 2 - paddingLeft - paddingRight;
     const pageMiddle = a6width / 2;
     const space = 2;
@@ -59,11 +62,11 @@ import { EUDGC, RecoveryEntry } from '../generated-files/dgc-combined-schema';
         }
 
         let _ci: string = '';
-        if(eudgc!.r) {
+        if (eudgc!.r) {
             _ci = eudgc!.r![0].ci;
-        } else if(eudgc!.t) {
+        } else if (eudgc!.t) {
             _ci = eudgc!.t![0].ci;
-        } else if(eudgc!.v) {
+        } else if (eudgc!.v) {
             _ci = eudgc!.v![0].ci;
         }
 
@@ -83,61 +86,17 @@ import { EUDGC, RecoveryEntry } from '../generated-files/dgc-combined-schema';
         // console.log(pdf.getFont());
 
         pdf.text("vierte Seite", a6width, a6height + 12);
-        pdf.text("erste Seite", 0, 0 + 12);
+        //pdf.text("erste Seite", 0, 0 + 12);
         //pdf.text("zweite Seite", a6width, 0 + 12);
         pdf.text("dritte Seite", 0, a6height + 12);
 
-        //Second page
-        var canvas: HTMLCanvasElement = qrCodeCanvasElement;
-        var img = canvas!.toDataURL("image/png,base64");
-        let canvasWidth = 192;
-        let centerLeft = (a6width - canvasWidth) / 2;
-        pdf.addImage(img, 'png', a6width + centerLeft, marginTop, canvasWidth, canvasWidth);
+        // First page
+        prepareFirstPage(marginTop, headerLineHeight, pdf, headerFontSize, a6width, marginBottom);
+        // End of first page
 
-        //For the labels on the left side
-        let xLeft = a6width;
-        let yLeft = marginTop + canvasWidth + lineHeight*2;
-        //For the variables on the right side
-        let xRight = a6width + pageMiddle;
-        let yRight = marginTop + canvasWidth + lineHeight*2;
-
-        pdf.setFontSize(11);
-
-        let lblSurname: string = 'Surname(s) and forename(s)';
-        lblSurname = pdf.splitTextToSize(lblSurname, lblLength);
-        pdf.text(lblSurname, xLeft, yLeft);
-        yLeft += lineHeight*2;
-        lblSurname = 'Nom(s) de famille et prénom(s)';
-        lblSurname = pdf.splitTextToSize(lblSurname, lblLength);
-        pdf.text(lblSurname, xLeft, yLeft);
-
-        let name = eudgc!.nam!.fnt + ' ' + eudgc!.nam!.gnt;
-        name = pdf.splitTextToSize(name, lblLength);
-        pdf.text(name, xRight, yRight);
-
-        yLeft += lineHeight*2 + space;
-        pdf.text('Date of birth', xLeft, yLeft);
-        yLeft+= lineHeight;
-        pdf.text('Date de naissance', xLeft, yLeft);
-
-        yRight += lineHeight*4 + space;
-        pdf.text(eudgc!.dob, xRight, yRight);
-
-        yLeft += lineHeight + space;
-        let lblci = 'Unique certificate identifier';
-        lblci = pdf.splitTextToSize(lblci, lblLength);
-        pdf.text(lblci, xLeft, yLeft);
-        yLeft += lineHeight;
-        lblci = 'Identifiant unique du certificat';
-        lblci = pdf.splitTextToSize(lblci, lblLength);
-        pdf.text(lblci, xLeft, yLeft);
-
-        yRight += lineHeight*2 + space;
-        _ci = pdf.splitTextToSize(_ci, lblLength);
-        pdf.text(_ci, xRight, yRight);
+        // Second page
+        prepareSecondPage(qrCodeCanvasElement, a6width, pdf, marginTop, lineHeight, pageMiddle, lblLength, eudgc, space, _ci);
         // End of second page
-
-        pdf.addImage(logo, 'png', 0, 0, 16, 16);
 
         pdf.save('edgcPdfTest');
     }, [qrCodeCanvasElement]);
@@ -152,4 +111,83 @@ const mm2point = (mm: number): number => {
     return mm * 2.83465;
 }
 
+const pixel2point = (pixel: number): number => {
+    return pixel * 0.75;
+}
+
 export default usePdfGenerator;
+
+const prepareSecondPage = (qrCodeCanvasElement: any, a6width: number, pdf: jsPDF, marginTop: number,
+    lineHeight: number, pageMiddle: number, lblLength: number,
+    eudgc: EUDGC | undefined, space: number, _ci: string) => {
+
+    var canvas: HTMLCanvasElement = qrCodeCanvasElement;
+    var img = canvas!.toDataURL("image/png,base64");
+    let canvasWidth = 192;
+    let centerLeft = (a6width - canvasWidth) / 2;
+    pdf.addImage(img, 'png', a6width + centerLeft, marginTop, canvasWidth, canvasWidth);
+
+    //For the labels on the left side
+    let xLeft = a6width;
+    let yLeft = marginTop + canvasWidth + lineHeight * 2;
+    //For the variables on the right side
+    let xRight = a6width + pageMiddle;
+    let yRight = marginTop + canvasWidth + lineHeight * 2;
+
+    pdf.setFontSize(11);
+
+    let lblSurname: string = 'Surname(s) and forename(s)';
+    lblSurname = pdf.splitTextToSize(lblSurname, lblLength);
+    pdf.text(lblSurname, xLeft, yLeft);
+    yLeft += lineHeight * 2;
+    lblSurname = 'Nom(s) de famille et prénom(s)';
+    lblSurname = pdf.splitTextToSize(lblSurname, lblLength);
+    pdf.text(lblSurname, xLeft, yLeft);
+
+    let name = eudgc!.nam!.fnt + ' ' + eudgc!.nam!.gnt;
+    name = pdf.splitTextToSize(name, lblLength);
+    pdf.text(name, xRight, yRight);
+
+    yLeft += lineHeight * 2 + space;
+    pdf.text('Date of birth', xLeft, yLeft);
+    yLeft += lineHeight;
+    pdf.text('Date de naissance', xLeft, yLeft);
+
+    yRight += lineHeight * 4 + space;
+    pdf.text(eudgc!.dob, xRight, yRight);
+
+    yLeft += lineHeight + space;
+    let lblci = 'Unique certificate identifier';
+    lblci = pdf.splitTextToSize(lblci, lblLength);
+    pdf.text(lblci, xLeft, yLeft);
+    yLeft += lineHeight;
+    lblci = 'Identifiant unique du certificat';
+    lblci = pdf.splitTextToSize(lblci, lblLength);
+    pdf.text(lblci, xLeft, yLeft);
+
+    yRight += lineHeight * 2 + space;
+    _ci = pdf.splitTextToSize(_ci, lblLength);
+    pdf.text(_ci, xRight, yRight);
+}
+const prepareFirstPage = (marginTop: number, headerLineHeight: number, pdf: jsPDF, headerFontSize: number,
+    a6width: number, marginBottom: number) => {
+    let x = 0;
+    let y = marginTop + headerLineHeight;
+    pdf.setFontSize(headerFontSize);
+    let header = 'DIGITAL GREEN CERTIFICATE';
+    let width = pdf.getTextWidth(header);
+    x = (a6width - width) / 2;
+    pdf.text(header, x, y);
+
+    header = 'CERTIFICAT VERT NUMÉRIQUE';
+    width = pdf.getTextWidth(header);
+    x = (a6width - width) / 2;
+    y += headerLineHeight;
+    pdf.text(header, x, y);
+
+    let logoWidth = 132.75;
+    let logoHeight = 88.5;
+    x = (a6width - logoWidth) / 2;
+    pdf.addImage(logo, 'png', x, a6width - marginBottom, logoWidth, logoHeight);
+}
+
